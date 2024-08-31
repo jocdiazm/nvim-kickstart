d265c57712698f270609a27474f1fe9fb7b189eb
Jose Carlos <jocdiazmuic@gmail.com>
2024-08-31 18:11:58 -0500

    adding configs to system-theme and treesiter-context

--- lua/custom/plugins/system-theme.lua
+++ lua/custom/plugins/system-theme.lua
@@ -1,15 +1,45 @@
+-- return {
+--   'f-person/auto-dark-mode.nvim',
+--   priority = 1200,
+--   config = {
+--     update_interval = 1000,
+--     set_dark_mode = function()
+--       vim.o.background = 'dark'
+--       vim.cmd 'colorscheme gruvbox-material'
+--     end,
+--     set_light_mode = function()
+--       vim.o.background = 'light'
+--       vim.cmd 'colorscheme rose-pine'
+--     end,
+--   },
+-- }
+--
 return {
   'f-person/auto-dark-mode.nvim',
   priority = 1200,
-  config = {
-    update_interval = 1000,
-    set_dark_mode = function()
-      vim.o.background = 'dark'
-      vim.cmd 'colorscheme gruvbox-material'
-    end,
-    set_light_mode = function()
-      vim.o.background = 'light'
-      vim.cmd 'colorscheme rose-pine'
-    end,
-  },
+  config = function()
+    local auto_dark_mode = require 'auto-dark-mode'
+
+    -- Define a function to apply custom highlights
+    local function apply_custom_highlights()
+      vim.api.nvim_set_hl(0, 'TreesitterContext', { bg = 'NONE', fg = vim.o.background == 'dark' and '#ebdbb2' or '#2e3440' })
+      vim.api.nvim_set_hl(0, 'TreesitterContextLineNumber', { fg = '#D2879B' })
+    end
+
+    auto_dark_mode.setup {
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.o.background = 'dark'
+        vim.cmd 'colorscheme gruvbox-material'
+        apply_custom_highlights()
+      end,
+      set_light_mode = function()
+        vim.o.background = 'light'
+        vim.cmd 'colorscheme rose-pine'
+        apply_custom_highlights()
+      end,
+    }
+
+    auto_dark_mode.init()
+  end,
 }
--- lua/custom/plugins/treesitter-context.lua
+++ lua/custom/plugins/treesitter-context.lua
@@ -1,4 +1,29 @@
 return {
   'nvim-treesitter/nvim-treesitter-context',
-  opts = {},
+  config = function()
+    local ts_context = require 'treesitter-context'
+    ts_context.setup {
+      enable = true,
+      max_lines = 3, -- Limit the maximum number of lines in the context window
+      trim_scope = 'outer', -- Trims the outermost context when the max_lines limit is exceeded
+      mode = 'topline', -- The line used to calculate the context is the first visible line
+      separator = nil, -- No separator line
+      zindex = 20, -- The Z-index of the context window
+      patterns = { -- Patterns for detecting the context
+        default = {
+          'class',
+          'function',
+          'method',
+          -- Add more patterns here if needed
+        },
+      },
+      exact_patterns = {}, -- exact patterns not needed in this case
+    }
+
+    -- Override the match function to ignore comments
+    function ts_context.match_callback(_, node, _)
+      local type = node:type()
+      return type ~= 'comment' and type ~= 'block_comment'
+    end
+  end,
 }
